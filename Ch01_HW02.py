51fb6abbd8d751bd7a7132550d035d8e32a27fc9
Amir Madankan <madankan.amir.2000@gmail.com>
2023-03-27 15:01:43 +0430

    chart created

--- Ch01_HW02.py
+++ Ch01_HW02.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 csv_file = open ("files/Ch01_HW02_csvFile.csv", 'r')
 values = csv_file.read()
 values = values.split(",")
@@ -7,4 +9,10 @@ for i in range(16, n-6, 11):
     values_close.append(float(values[i]))
 
 values_close.reverse()
-print(values_close)
+
+day = list(range(1,len(values_close)+1))
+plt.plot(day, values_close)
+plt.title("fameli from 2007 to 2022")
+plt.xlabel("day")
+plt.ylabel("price")
+plt.show()
