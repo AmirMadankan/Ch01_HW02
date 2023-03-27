e09c2bbac05f2310915161c8414d0824889036b5
Amir Madankan <madankan.amir.2000@gmail.com>
2023-03-27 15:45:21 +0430

    derivative created and plotted

--- Ch01_HW02.py
+++ Ch01_HW02.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 csv_file = open ("files/Ch01_HW02_csvFile.csv", 'r')
 values = csv_file.read()
 values = values.split(",")
+
 n = len(values)
 values_close = []
 for i in range(16, n-6, 11):
@@ -10,8 +11,16 @@ for i in range(16, n-6, 11):
 
 values_close.reverse()
 
-day = list(range(1,len(values_close)+1))
-plt.plot(day, values_close)
+m = len(values_close)
+dy = []
+for i in range(m-1):
+    dy.append(values_close[i+1] - values_close [i])
+
+yprime = dy
+
+plt.plot(values_close, label="chart")
+plt.plot(yprime,label="dervative")
+plt.legend()
 plt.title("fameli from 2007 to 2022")
 plt.xlabel("day")
 plt.ylabel("price")
